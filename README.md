bd8838c5a1c11f6831bf6911e0d93a0d4f57c709
TitoEt-ATU <L00203089@atu.ie>
2025-11-17 23:44:37 +0000

    Defined variables and outputs, follow tutorial for main.tf in vpc module, linked in readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # CA2-TerraformLab
 
 Create a project using terraform for the following to host an application across clusters in twoavailability zones. You may use an architecture of your choice but you should explain yourdecisions as you go.Make sure to host your solution on github making the repository publicly available. Ensure thatyou show multiple prs to the repo to show continuous improvement.A single cluster example architecture might looklike this, but don’t forget to consider the loadbalancer, networking, etc.A multi-cluster architecture is different. A singlecluster is required as a minimal solution.
+
+
+https://youtu.be/pvhdT6Z92QU?si=SLeeTYFwzuqQCznw - VPC Tut
+https://www.devopshint.com/create-vpc-in-aws-using-terraform-modules/
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,40 +1,91 @@
-#from terraform module tutorial, key components
-provider "aws" {
-  region = "eu-west-1"
+resource "aws_vpc" "this" {
+  cidr_block = var.cidr_block
+  tags = merge(
+    var.tags,
+    { Name = "${var.name}-vpc" }
+  )
 }
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+  tags   = var.tags
+}
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets)
 
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.public_subnets[count.index]
+  availability_zone = var.azs[count.index]
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.name}-public-${count.index}" }
+  )
+}
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets)
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.azs[count.index]
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.name}-private-${count.index}" }
+  )
+}
 resource "aws_eip" "nat" {
-  count = 3
-  domain = "vpc"
+    domain = "vpc"
+    tags = merge(
+      var.tags,
+      { Name = "${var.name}-nat-eip" }
+    )
+    depends_on = [aws_internet_gateway.igw]
 }
-
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
-  single_nat_gateway  = false
-  reuse_nat_ips = true                   
-  external_nat_ip_ids = "${aws_eip.nat.*.id}" 
-  create_vpc = false
-  create_database_subnet_group = true
-  create_database_subnet_route_table = true
-  create_database_internet_gateway_route = true
-  enable_public_redshift = true
-
-  enable_dns_hostnames = true
-  enable_dns_support = true
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+  tags = merge(
+    var.tags,
+    { Name = "${var.name}-nat" }
+  )
+    depends_on = [aws_internet_gateway.igw]
+}
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
+
+    tags = merge(
+        var.tags,
+        { Name = "${var.name}-public-rt" }
+    )
+}
+resource "aws_route_table_association" "public_assoc" {
+  count = length(var.public_subnets)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
 }
 
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.name}-private-rt" }
+  )
+}
+resource "aws_route_table_association" "private_assoc" {
+  count = length(var.private_subnets)
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
